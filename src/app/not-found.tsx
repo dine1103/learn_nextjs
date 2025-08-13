3ed8b5abe88ff2d8a4b6b537bab4ba34b23368dd
dine1103 <duy.nguyen@aegistack.com>
2025-08-13 14:27:27 +0700

    video-10-not-found-page-done

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,7 @@
 export default function NotFound(){
     return (
         <div>
-            <h2>Page not found</h2>
-            <p>Could not find requested resource</p>
+            <h2>Review not found</h2>
         </div>
     )
 }
