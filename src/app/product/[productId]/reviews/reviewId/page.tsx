4ec72682c1ca0a0e0d44a77feb9462eb6e7bdca9
dine1103 <duy.nguyen@aegistack.com>
2025-08-13 09:46:14 +0700

    learn next.js

---
+++ src/app/product/[productId]/reviews/reviewId/page.tsx
@@ -0,0 +1,12 @@
+export default async function ProductReview({
+    params,
+}:{
+        params: {productId: string; reviewId: string};
+}) {
+    const {productId, reviewId} = params;
+    return (
+    <h1>
+        Review {reviewId} for product {productId}
+    </h1>
+);
+}
