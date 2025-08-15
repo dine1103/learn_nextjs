47ae40234b589e3f7eab36ca0bc34a0fe3f81825
dine1103 <duy.nguyen@aegistack.com>
2025-08-15 09:43:39 +0700

    test010

---
+++ src/app/hihihi/page.tsx
@@ -0,0 +1,5 @@
+export default async function Hihi(
+    params,
+)}:{
+    return <h1>Nothing</h1>
+}
--- src/app/test01/page.tsx
+++ src/app/test01/page.tsx
@@ -1,1 +1,3 @@
-gdhgfsiuhfguhsguhghg
+export default function Test01 (){
+    return <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum excepturi ex adipisci perspiciatis et officiis placeat minus nobis deleniti. Repudiandae totam tempore facere culpa quidem, odit corporis velit rem reiciendis.</p>
+}
