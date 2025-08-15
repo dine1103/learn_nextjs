f7c08183aefe3a4591d8b7362277537dfa26a546
dine1103 <duy.nguyen@aegistack.com>
2025-08-15 11:05:34 +0700

    g

--- src/app/test01/page.tsx
+++ src/app/test01/page.tsx
@@ -1,3 +1,6 @@
 export default function Test01 (){
-    return <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum excepturi ex adipisci perspiciatis et officiis placeat minus nobis deleniti. Repudiandae totam tempore facere culpa quidem, odit corporis velit rem reiciendis.</p>
+    return <>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum excepturi ex adipisci perspiciatis et officiis placeat minus nobis deleniti. Repudiandae totam tempore facere culpa quidem, odit corporis velit rem reiciendis.</p>
+    <h1>D</h1>
+    </>
 }
